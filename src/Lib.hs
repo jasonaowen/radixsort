f303a52fc30fcb400f9b284a9cec78ab428e5c47
Jason Owen <jason.a.owen@gmail.com>
2017-01-13 15:00:38 -0500

    implement base-2 radix sort

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
 module Lib
     ( someFunc
+    , radixsort
+    , radixsortIteration
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+radixsort :: Integral a => [a] -> [a]
+radixsort [] = []
+radixsort [x] = [x]
+radixsort xs = radixsortIteration maxDivisor 1 [[], []] xs
+  where maxDivisor = floor $ logBase 2 $ fromIntegral $ maximum xs
+
+radixsortIteration :: Integral a => a -> a -> [[a]] -> [a] -> [a]
+radixsortIteration maxDivisor divisor [evens, odds] []
+  | divisor > maxDivisor + 1 = evens ++ odds
+  | otherwise                = radixsortIteration maxDivisor (divisor * 2) [[], []] (evens ++ odds)
+radixsortIteration maxDivisor divisor [evens, odds] (x:xs)
+  | even $ div x divisor = radixsortIteration maxDivisor divisor [evens ++ [x], odds] xs
+  | otherwise            = radixsortIteration maxDivisor divisor [evens, odds ++ [x]] xs
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,5 +6,19 @@ import Lib
 main :: IO ()
 main = hspec $ do
   describe "radixsort library" $ do
-    it "runs the tests" $ do
-      True `shouldBe` True
+    it "sorts an empty list" $ do
+      radixsort ([]::[Int]) `shouldBe` []
+    it "sorts a single-element list" $ do
+      radixsort [0] `shouldBe` [0]
+    it "sorts a two-element in-order list" $ do
+      radixsort [0, 1] `shouldBe` [0, 1]
+    it "sorts a two-element out-of-order list" $ do
+      radixsort [1, 0] `shouldBe` [0, 1]
+    it "sorts a list with duplicate elements" $ do
+      radixsort [1, 0, 1] `shouldBe` [0, 1, 1]
+    it "sorts a list with larger log_2 values" $ do
+      radixsort [2, 1] `shouldBe` [1, 2]
+    it "sorts a list with even larger log_2 values" $ do
+      radixsort [2, 8] `shouldBe` [2, 8]
+    it "sorts a list with several values" $ do
+      radixsort [2, 1, 12, 7, 3, 8] `shouldBe` [1, 2, 3, 7, 8, 12]
