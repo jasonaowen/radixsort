d181d936ccf1ac3ff40e973b0afbbef5af723b17
Jason Owen <jason.a.owen@gmail.com>
2017-01-13 15:27:00 -0500

    use an arbitrary number of buckets

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,21 +2,30 @@ module Lib
     ( someFunc
     , radixsort
     , radixsortIteration
+    , appendToListAtIndex
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-radixsort :: Integral a => [a] -> [a]
-radixsort [] = []
-radixsort [x] = [x]
-radixsort xs = radixsortIteration maxDivisor 1 [[], []] xs
-  where maxDivisor = floor $ logBase 2 $ fromIntegral $ maximum xs
+radixsort :: Integral a => a -> [a] -> [a]
+radixsort _ [] = []
+radixsort _ [x] = [x]
+radixsort base xs = radixsortIteration base maxDivisor 1 buckets xs
+  where maxDivisor = floor $ logBase (fromIntegral base) $ fromIntegral $ maximum xs
+        buckets = take (fromIntegral base) $ repeat []
 
-radixsortIteration :: Integral a => a -> a -> [[a]] -> [a] -> [a]
-radixsortIteration maxDivisor divisor [evens, odds] []
-  | divisor > maxDivisor + 1 = evens ++ odds
-  | otherwise                = radixsortIteration maxDivisor (divisor * 2) [[], []] (evens ++ odds)
-radixsortIteration maxDivisor divisor [evens, odds] (x:xs)
-  | even $ div x divisor = radixsortIteration maxDivisor divisor [evens ++ [x], odds] xs
-  | otherwise            = radixsortIteration maxDivisor divisor [evens, odds ++ [x]] xs
+radixsortIteration :: Integral a => a -> a -> a -> [[a]] -> [a] -> [a]
+radixsortIteration base maxDivisor divisor buckets []
+  | divisor > maxDivisor + 1 = foldl (++) [] buckets
+  | otherwise                = radixsortIteration base maxDivisor (divisor * base) base_buckets (foldl (++) [] buckets)
+  where base_buckets = take (fromIntegral base) $ repeat []
+radixsortIteration base maxDivisor divisor buckets (x:xs)
+  = radixsortIteration base maxDivisor divisor (appendToListAtIndex buckets bucket x) xs
+  where bucket = rem (div x divisor) base
+
+appendToListAtIndex :: Integral a => [[a]] -> a -> a -> [[a]]
+appendToListAtIndex (list:lists) 0 value
+  = (list ++ [value]):lists
+appendToListAtIndex (list:lists) index value
+  = list:(appendToListAtIndex lists (index - 1) value)
--- test/Spec.hs
+++ test/Spec.hs
@@ -7,18 +7,31 @@ main :: IO ()
 main = hspec $ do
   describe "radixsort library" $ do
     it "sorts an empty list" $ do
-      radixsort ([]::[Int]) `shouldBe` []
+      radixsort 2 ([]::[Int]) `shouldBe` []
     it "sorts a single-element list" $ do
-      radixsort [0] `shouldBe` [0]
+      radixsort 2 [0] `shouldBe` [0]
     it "sorts a two-element in-order list" $ do
-      radixsort [0, 1] `shouldBe` [0, 1]
+      radixsort 2 [0, 1] `shouldBe` [0, 1]
     it "sorts a two-element out-of-order list" $ do
-      radixsort [1, 0] `shouldBe` [0, 1]
+      radixsort 2 [1, 0] `shouldBe` [0, 1]
     it "sorts a list with duplicate elements" $ do
-      radixsort [1, 0, 1] `shouldBe` [0, 1, 1]
+      radixsort 2 [1, 0, 1] `shouldBe` [0, 1, 1]
     it "sorts a list with larger log_2 values" $ do
-      radixsort [2, 1] `shouldBe` [1, 2]
+      radixsort 2 [2, 1] `shouldBe` [1, 2]
     it "sorts a list with even larger log_2 values" $ do
-      radixsort [2, 8] `shouldBe` [2, 8]
+      radixsort 2 [2, 8] `shouldBe` [2, 8]
     it "sorts a list with several values" $ do
-      radixsort [2, 1, 12, 7, 3, 8] `shouldBe` [1, 2, 3, 7, 8, 12]
+      radixsort 2 [2, 1, 12, 7, 3, 8] `shouldBe` [1, 2, 3, 7, 8, 12]
+    it "sorts a list with 3 buckets" $ do
+      radixsort 3 [2, 1, 12, 7, 3, 8] `shouldBe` [1, 2, 3, 7, 8, 12]
+    it "sorts a list with 4 buckets" $ do
+      radixsort 4 [2, 1, 12, 7, 3, 8] `shouldBe` [1, 2, 3, 7, 8, 12]
+    it "sorts a large list with large numbers using many buckets" $ do
+      radixsort 50 [1024,1023..1] `shouldBe` [1..1024]
+  describe "list appending" $ do
+    it "appends to the first list (index 0)" $ do
+      appendToListAtIndex [[]] 0 1 `shouldBe` [[1]]
+    it "appends to the second list (index 1)" $ do
+      appendToListAtIndex [[], []] 1 1 `shouldBe` [[], [1]]
+    it "appends to a non-empty list" $ do
+      appendToListAtIndex [[0]] 0 1 `shouldBe` [[0, 1]]
